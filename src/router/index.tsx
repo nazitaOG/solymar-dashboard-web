ad0c87cee28cb44e95cab2165d4511ea82ec05aa
nazitaOG <lopeznazarenoo@gmail.com>
2026-01-28 17:53:31 -0300

    hydrate loader

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,16 @@
-import { createBrowserRouter, RouteObject } from "react-router";
+import { createBrowserRouter, RouteObject } from "react-router"; // O react-router-dom
+import { Outlet } from "react-router"; // 👈 Necesitas importar Outlet
 import { routes } from "./routes";
+import { FullPageLoader } from "@/components/FullPageLoader"; // 👈 Importa tu loader
 
-export const router = createBrowserRouter(routes satisfies RouteObject[]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    // El Outlet es un "hueco" donde se renderizan tus rutas hijas (routes)
+    element: <Outlet />, 
+    // LA SOLUCIÓN AL WARNING
+    hydrateFallbackElement: <FullPageLoader />, 
+    // Tus rutas originales pasan a ser hijas de esta ruta raíz
+    children: routes,
+  }
+] satisfies RouteObject[]);
